a9d8374ba415659676a3da4ccfdd4c72c0299f84
Omar Estrada Diaz <omarito4857@gmail.com>
2020-05-15 01:43:17 -0500

    adding needed libs

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+import { Text } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Text>helhellohellohellohellohellohe
+        llohellohellohellohellohellohellohe
+        llohellohellohellohellohellohellohell
+        ohellohellohellohellohellohellohelloh
+        ellohellohellohellohellohellohellohel
+        lohellohellohellohellohellohellohello
+          hellolo</Text>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
